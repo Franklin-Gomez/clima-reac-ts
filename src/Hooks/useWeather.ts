3c815bb6f401ff0542244329efe2c9db5fb486dc
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-26 09:15:15 -0500

    add .env , finish first call

--- src/Hooks/useWeather.ts
+++ src/Hooks/useWeather.ts
@@ -5,13 +5,13 @@ export default function useWeather() {
 
     const fetchWeather = async (search : SearchType) => { 
         
-        const appid = '5e13f47a14d9e3a1a3a6f7db87fe5c03';
+        const appid = import.meta.env.VITE_API_KEY;
 
         try {
             const url = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&&appid=${appid}`
             
             // get es default pero por ilustracion lo pondremoos
-            const data = await axios ( url , { method : 'get'})
+            const {data} = await axios ( url , { method : 'get'})
             
 
         } catch (error) {
