2147776dded052faba20f6668a20b27d9e2f0ae6
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-26 09:39:20 -0500

    add second API call

--- src/Hooks/useWeather.ts
+++ src/Hooks/useWeather.ts
@@ -6,13 +6,21 @@ export default function useWeather() {
     const fetchWeather = async (search : SearchType) => { 
         
         const appid = import.meta.env.VITE_API_KEY;
-
+        
         try {
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&&appid=${appid}`
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&&appid=${appid}`
             
             // get es default pero por ilustracion lo pondremoos
-            const {data} = await axios ( url , { method : 'get'})
+            const {data} = await axios ( geoUrl , { method : 'get'})
+
+            const lat = data[0].lat
+            const lon = data[0].lon
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`
             
+            // asignamos la variable data el nombre de weatherResult
+            const {data : weatherResult } = await axios( weatherUrl , { method : 'get'});
+
 
         } catch (error) {
             console.log( error)
