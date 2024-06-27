2dced6a3a70507915b2063aeace1bec9754155c4
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-26 22:19:57 -0500

    add styles to wheaterDetails components

--- src/components/WeatherDetails/WeatherDetails.tsx
+++ src/components/WeatherDetails/WeatherDetails.tsx
@@ -11,9 +11,9 @@ export default function WeatherDetails( {weather} : weatherDetailsProps) {
     return (
         <div className={styles.container}>
             <h2> Clima de : { weather.name }</h2>
-            <p> temperatura : { KelvinToCelsius( weather.main.temp )}&deg;C</p>
+            <p className={styles.current}> { KelvinToCelsius( weather.main.temp )}&deg;C</p>
 
-            <div>
+            <div className={styles.temperatures}>
                 <p>Max : <span>{ KelvinToCelsius( weather.main.temp_max )}&deg;C</span></p>
                 <p>Min : <span>{ KelvinToCelsius( weather.main.temp_min )}&deg;C</span></p>
             </div>
