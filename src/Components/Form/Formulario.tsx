26d0190f26f760478944feb16f12b8b271053879
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-01 09:39:46 -0500

    get the values form

--- src/Components/Form/Formulario.tsx
+++ src/Components/Form/Formulario.tsx
@@ -1,28 +1,60 @@
+import { useState } from "react"
 import { countries } from "../../db/data"
 import style from './Formulario.module.css'
 
 export default function Formulario() {
-  return (
-    <form className={style.container}>
-        <div className={style.input}>
-            <label htmlFor="">Ciudad : </label>
-            <input type="text"  placeholder="Ciudad" className={style.campo}/>
-        </div>
 
-        <div className={style.input}>
-            <label>Pais : </label>
-            <select name="" id="" className={style.campo}>
-                <option value=""> -- Seleccione el Pais -- </option>
 
-                { countries.map ((country) => (
-                    <option value={country.code}>{country.name}</option>
-                ))}
+    const [ country , setCountry ] = useState( { 
+        city : '',
+        country : ''
+    })
 
-            </select>
-        </div>
+    const handleChange = ( e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>  ) => { 
+        setCountry (
+            {
+                ...country,
+                [e.target.name] :  e.target.value
+            }
+        )
+    }
 
-        <input type="submit" value='Buscar Clima' className={style.input3} />
-    </form>
-    
-  )
+
+    return (
+        <form className={style.container}>
+            <div className={style.input}>
+                <label htmlFor="">Ciudad : </label>
+                <input 
+                    type="text"  
+                    name="city"
+                    placeholder="Ciudad" 
+                    className={style.campo}
+                    onChange={ handleChange }    
+                />
+            </div>
+
+            <div className={style.input}>
+                <label>Pais : </label>
+                <select 
+                    name="country" 
+                    id="" 
+                    className={style.campo} 
+                    onChange={ handleChange }
+                >
+                    <option value=""> -- Seleccione el Pais -- </option>
+
+                    { countries.map ((country) => (
+                        <option 
+                            value={country.code}
+                            key={country.code}
+                        >{country.name}</option>
+                    ))}
+
+                </select>
+            </div>
+
+            <input type="submit" value='Buscar Clima' className={style.input3} />
+        </form>
+
+    )
 }
